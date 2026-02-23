5acbbd70481a2d0719aff95b747209e95e03c02e
Jamilly Ferreira <jamillyferreira039@gmail.com>
2026-02-23 10:12:20 -0400

    chore: add initial project setup and dev properties

---
+++ src/main/java/com/jamillyferreira/veterinaryclinic/VeterinaryClinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.jamillyferreira.veterinaryclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryClinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamillyferreira/veterinaryclinic/VeterinaryClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamillyferreira.veterinaryclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
